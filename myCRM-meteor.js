6a0aaf13abbdc1d63f1fad736d137dcedb12364f
zemigsan <zemigsan@gmail.com>
2015-07-12 22:06:55 +0100

    added first calendar algorithm
    - creates a calendar structure splitting the projects through the days

--- myCRM-meteor.js
+++ myCRM-meteor.js
@@ -1,9 +1,13 @@
 Projects = new Mongo.Collection("projects");
+Calendars = new Mongo.Collection("calendars");
 
 if (Meteor.isClient) {
 
   // this allows after descontinue the autopublish
   Meteor.subscribe("projects");
+  Meteor.subscribe("calendars");
+
+
 
   // to add the private function
   Template.project.helpers({
@@ -12,21 +16,19 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.registerHelper('formatDate', function(date) {
+    return moment(date).format('dddd (MMM Do)');
+  });
+
+
+
 
   // This code only runs on the client
   Template.body.helpers({
     projects: function () {
       if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
-        return Projects.find({
-          checked: {
-            $ne: true
-          }
-        }, {
-          sort: {
-            deadline: -1
-          }
-        });
+        return Projects.find({ checked: { $ne: true }}, {sort: { deadline: -1 } });
       } else {
         // Otherwise, return all of the tasks
         return Projects.find({}, {
@@ -35,6 +37,10 @@ if (Meteor.isClient) {
           }
         });
       }
+    },
+    calendars: function() {
+      return Calendars.findOne().days;
+
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
@@ -59,6 +65,7 @@ if (Meteor.isClient) {
       var duration = event.target.duration.value;
 
       Meteor.call("addProject", projectname, deadline, duration);
+      Meteor.call("updateCalendar");
 
 
       // Clear form
@@ -69,6 +76,10 @@ if (Meteor.isClient) {
     },
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
+    },
+    "click .toggle-private": function () {
+      //Meteor.call("setPrivate", this._id, !this.private);
+      Meteor.call("updateCalendar");
     }
   });
 
@@ -79,9 +90,6 @@ if (Meteor.isClient) {
     },
     "click .delete": function () {
       Meteor.call("deleteProject", this._id);
-    },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
 
@@ -96,17 +104,17 @@ if (Meteor.isClient) {
 //So why do we want to define our methods on the client and on the server?
 //We do this to enable a feature called latency compensation.
 Meteor.methods({
-  addProject: function (text, deadline, duration) {
+  addProject: function (name, deadline, duration) {
     // Make sure the user is logged in before inserting a task
     if (!Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
     Projects.insert({
-      text: text,
-      duration: duration,
-      deadline: deadline,
-      createdAt: new Date(),
+      name: name,
+      duration: parseInt(duration),
+      deadline: new Date(deadline),
+      createdAt:new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
     });
@@ -135,6 +143,89 @@ Meteor.methods({
         private: setToPrivate
       }
     });
+  },
+
+  updateCalendar: function () {
+    //remove current calendars
+    Calendars.remove({user: this.userId});
+    // get the future projects
+    var futureProjects = Projects.find( { "deadline" : { "$gte" : new Date()  } },
+    {sort: { deadline: 1 } });
+
+    var totalHours=0;
+
+    var currentDayHours = 0;
+    var currentDate = { "date" : new Date(), "projects" : [] };
+    var document = { user: this.userId, total: totalHours, days : []};
+
+
+    //Calendars.update({user: this.userId},  {$push: { days: {total: totalHours}}});
+
+    futureProjects.forEach(function(project){
+
+      totalHours = totalHours + project.duration;
+
+      var hoursLeft=project.duration;
+      while((hoursLeft+currentDayHours)>8) {
+        var hours = 8-currentDayHours;
+        currentDate.projects.push(  {"name": project.name, "hours": hours});
+        /*if(currentDayHours = 0) {
+        Calendars.update({user: this.userId},
+        { $push: { days: {"date" : currentDate,
+        "projects" : [
+        {"name": project.name, "hours": hours}]
+        }}});
+        } else {
+        //if already exists
+        Calendars.update({"user": this.userId, "days.date" : currentDate },
+        { "$push" : { "days.$.projects" :
+        {"name": project.name,"hours": hours}
+        }});
+        }*/
+
+        // clears the day
+        hoursLeft = hoursLeft - hours;
+        currentDayHours =0;
+        document.days.push(currentDate);
+        currentDate = { "date" : new Date(moment(currentDate.date).add(1,'days').toISOString()),
+        "projects" : [] };
+      }
+
+      /*if(currentDayHours = 0) {
+      Calendars.update({user: this.userId},
+      { $push: { days: {"date" : currentDate,
+      "projects" : [
+      {"name": project.name, "hours": hoursLeft}]
+      }}});
+      currentDayHours = hoursLeft;
+      } else {
+      //if already exists
+      Calendars.update({"user": this.userId, "days.date" : currentDate },
+      { "$push" : { "days.$.projects" :
+      {"name": project.name,"hours": hoursLeft}
+      }});
+      }*/
+      if (hoursLeft > 0) {
+        currentDate.projects.push(  {"name": project.name, "hours": hoursLeft});
+        currentDayHours = currentDayHours + hoursLeft;
+      }
+
+
+
+      //in case of new day
+      if(currentDayHours == 8) {
+        currentDayHours =0;
+        document.days.push(currentDate);
+        currentDate = { "date" : new Date(moment(currentDate.date).add(1,'days').toISOString()),
+        "projects" : [] };
+      }
+    });
+
+    // the last
+    if (currentDate.projects.length >0) document.days.push(currentDate);
+    Calendars.insert(document);
+
+
   }
 });
 
@@ -142,6 +233,9 @@ Meteor.methods({
 // Modify the publish statement
 // Only publish tasks that are public or belong to the current user
 if (Meteor.isServer) {
+  Meteor.publish("calendars", function () {
+    return Calendars.find({user: this.userId});
+  });
   Meteor.publish("projects", function () {
     return Projects.find({
 
@@ -159,11 +253,10 @@ if (Meteor.isServer) {
           owner: this.userId
         }
       ]
-
-
-
-
     });
   });
+
+
+
 
 }
